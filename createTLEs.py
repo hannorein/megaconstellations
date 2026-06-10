b913717222b9ea2edc6e4aaad695a2fdec6f1501
Hanno Rein <hanno@hanno-rein.de>
2026-06-10 18:22:07 -0400

    createTLE script

---
+++ createTLEs.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.insert(0,"./model")
+import mega as m
+import numpy as np
+
+def TLE(n, inc, Omega, M):
+    TLE = """STARLINK-FUTURE
+1 49460U 21104BE  21337.58334491  .00068382  00000+0  48921-6 0  9997
+2 49460 %8.4f %8.4f 0000000   0.0000 %8.4f %11.8f  4453
+""" %(inc,Omega,M, n)
+    return TLE
+count = 0
+di = {"Starlink":"starlinkfuture","OneWeb":"onewebfuture", "StarNet/GW":"starnetfuture","Kuiper":"kuiperfuture", "SXODC":"sxodc"}
+for k in di:
+    name = di[k]
+    ICs = m.constellations_all[k]
+    with open(name+".txt", "w") as f:
+        for IC in ICs:
+            nplanes = IC["NPLANES"]
+            nsat = IC["SATPP"]
+            a = IC["ALT"]*1000.0 + m.REarth ## in m
+            GM = 2.9755363e+24 ## m^3/day^2
+            n = np.sqrt(GM/(a*a*a))/np.pi/2.
+            Omegas = np.linspace(0.,2.*np.pi*nplanes) + 2.*np.pi*np.random.random()
+            for i, Omega in enumerate(Omegas):
+                Ms = np.linspace(0.,2.*np.pi,nsat) + 2.*np.pi/nsat*0.25*np.random.normal(size=nsat)
+                Omega = np.fmod(Omega/np.pi*180.+360.,360.)
+                for M in Ms:
+                    M = np.fmod(M/np.pi*180.+360.,360.)
+                    t = TLE(n=n, inc=IC["INC"], Omega=Omega, M=M)
+                    count +=1
+                    f.write(t)
+    print(count)
