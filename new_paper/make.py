09fddd49693632ec60cb7d851f980213dbe3e8eb
Hanno Rein <hanno@hanno-rein.de>
2026-07-29 22:17:32 +0000

    added cache script

---
+++ new_paper/make.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+import rebound
+import sys
+sys.path.insert(0,'../model')
+import mega
+for name in ["SXODC", "Sunrise", "Stampede"]:
+    constellations = {name: mega.constellations_all[name]}
+    os.system("rm mega_"+name+".bin")
+    sims = mega.get_simulations(constellations)
+    print("Done",name,": N=", sims[name].N)
