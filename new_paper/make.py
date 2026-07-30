a0f83d74bc9808c70028919678bc0fb5d991bac7
Hanno Rein <hanno@hanno-rein.de>
2026-07-30 12:17:13 +0000

    max

--- new_paper/make.py
+++ new_paper/make.py
@@ -5,6 +5,7 @@ import sys
 sys.path.insert(0,'../model')
 import mega
 for name in ["SXODC", "Sunrise", "Stampede"]:
+#for name in ["Sunrise", "Stampede"]:
     constellations = {name: mega.constellations_all[name]}
     os.system("rm mega_"+name+".bin")
     sims = mega.get_simulations(constellations)
