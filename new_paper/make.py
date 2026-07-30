bbbf83829a5cd17500bddb02f9a726d8599d9513
Hanno Rein <hanno@hanno-rein.de>
2026-07-30 19:42:18 +0000

    fixes length of night sign

--- new_paper/make.py
+++ new_paper/make.py
@@ -4,8 +4,8 @@ import rebound
 import sys
 sys.path.insert(0,'../model')
 import mega
-for name in ["SXODC", "Sunrise", "Stampede"]:
-#for name in ["Sunrise", "Stampede"]:
+#for name in ["SXODC", "Sunrise", "Stampede"]:
+for name in ["Stampede"]:
     constellations = {name: mega.constellations_all[name]}
     os.system("rm mega_"+name+".bin")
     sims = mega.get_simulations(constellations)
