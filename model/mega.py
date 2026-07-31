71031e44ca03272c3910c7bfc58b8ce14f4a8592
Hanno Rein <hanno@hanno-rein.de>
2026-07-31 00:00:01 +0000

    plots with k=0.15

--- model/mega.py
+++ model/mega.py
@@ -370,7 +370,7 @@ def length_of_night(month,latitude, p=0):
         return 24.0
     return 24./np.pi * np.arccos(arccosarg)
 
-def get_stereographic_data(sims, latitude=0., month=0., hour=0., albedo=0.2, area=4., airmassCoeff=0.2, randomCoeff=0.5, elevation_cut = 0):
+def get_stereographic_data(sims, latitude=0., month=0., hour=0., albedo=0.2, area=4., airmassCoeff=0.15, randomCoeff=0.5, elevation_cut = 0):
     # latitude in degrees
     # month in months from spring euquinox
     # hours in hours since midnight
